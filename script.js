524b71bfb8d5edb8898568c445379d0dc66992c9
1nsulin <Tommy-Saliery@yandex.ru>
2020-05-15 09:03:27 +0400

    Выполнил задание 4

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ function start() {
 
 }
 
-//start();
+start();
 
 let appData = {
-    budge: money,
+    budget: money,
     timeData: time,
     expenses : {},
     optionalExpenses: {},
@@ -42,20 +42,29 @@ function chooseExpenses() {
 
 //chooseExpenses();
 
+function detectDayBudget(){
 
-appData.moneyPerDay = +(appData.budge / 30).toFixed(1);
-
-if( appData.moneyPerDay <= 100 ){
-    alert("Очень маленький дневной бюджет");
-} else if( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
-    alert("Бюджет среднего размера");
-} else if ( appData.moneyPerDay >= 2000){
-    alert("Жируешь дядя");
-} else {
-    alert("Произошла пижня вселенского масштаба");
+    appData.moneyPerDay = +(appData.budget / 30).toFixed(1);
+    alert(appData.moneyPerDay);
 };
+detectDayBudget();
 
-console.log(appData.moneyPerDay);
+
+function detectLevel(){
+
+    if( appData.moneyPerDay <= 100 ){
+        alert("Очень маленький дневной бюджет");
+    } else if( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ){
+        alert("Бюджет среднего размера");
+    } else if ( appData.moneyPerDay >= 2000){
+        alert("Жируешь дядя");
+    } else {
+        alert("Произошла пижня вселенского масштаба");
+    };
+
+};
+detectLevel();
+
 
 function checkSavings() {
     if(appData.savings == true){
@@ -65,10 +74,23 @@ function checkSavings() {
     appData.monthIncome = save/100/12*percent;
     alert("Доход с вашего депозита: " + appData.monthIncome + "$ в месяц");
     }
-}
+};
+//checkSavings();
 
-checkSavings();
-alert("lol")
+function chooseOptExpenses(){
+
+    for(let i = 1; i<=3; i++){
+        let a = prompt("Статья необязательных расходов", "");
+
+        if(a != null && a != ""){
+            appData.optionalExpenses[i] = a;
+        } else{
+            i--;
+        };
+    }
+
+}
+//chooseOptExpenses();
 
 
 
